94214b5606114a87594f1a3ececd6d91bdd28895
Brian Flett <brian.g.flett@gmail.com>
2025-03-18 23:14:09 -0400

    Added Submission Entity

---
+++ CRM/Mascode/BAO/MascodeSubmission.php
@@ -0,0 +1,7 @@
+<?php
+
+use CRM_Mascode_ExtensionUtil as E;
+
+class CRM_Mascode_BAO_MascodeSubmission extends CRM_Mascode_DAO_MascodeSubmission {
+
+}
---
+++ Civi/Api4/MascodeSubmission.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * MascodeSubmission entity.
+ *
+ * Provided by the mascode extension.
+ *
+ * @package Civi\Api4
+ */
+class MascodeSubmission extends Generic\DAOEntity {
+
+}
