5356d190e2686ec348270ba961496718a6554f15
Brian Flett <brian.g.flett@gmail.com>
2025-04-18 10:14:47 -0400

    Comment out to simplify for now

--- mascode.php
+++ mascode.php
@@ -13,15 +13,57 @@
 require_once 'mascode.civix.php';
 
 // Load Composer autoload if present
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-  require_once __DIR__ . '/vendor/autoload.php';
-}
+// if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+//   require_once __DIR__ . '/vendor/autoload.php';
+// }
 
 use CRM_Mascode_ExtensionUtil as E;
 
-use Civi\Mascode\Event\CasePreListener;
-use Civi\Mascode\Event\CasePostListener;
-use Civi\Mascode\Event\CaseSummaryListener;
+// use Civi\Mascode\Event\CasePreListener;
+// use Civi\Mascode\Event\CasePostListener;
+// use Civi\Mascode\Event\CaseSummaryListener;
+
+/**
+ * Implement hook_civicrm_caseSummary() inline for now and comment everything else out.
+ */
+function mascode_civicrm_caseSummary($caseId)
+{
+  if (empty($caseId)) {
+    return;
+  };
+
+  try {
+      $case = \Civi\Api4\CiviCase::get()
+          ->addWhere('id', '=', $caseId)
+          ->setLimit(1)
+          ->execute()
+          ->first();
+
+      if (!$case) {
+          return [];
+      }
+    } catch (\Exception $e) {
+        return [];
+    }
+
+    $formattedEndDate = !empty($case['end_date'])
+        ? CRM_Utils_Date::customFormat($case['end_date'])
+        : '';
+
+    //   Instead of returning the array of values and styling trough CSS
+    //   return the HTML itself so you can reference the civi styling classes
+    //   \CRM_Core_Resources::singleton()->addStyleFile('mascode', 'css/extras.css');
+
+    $html = '<table class="report crm-entity case-summary" style="margin-top: 1em;"><tbody><tr>';
+    $html .= '<td class="label"><span class="crm-case-summary-label">End Date:</span>&nbsp;' . $formattedEndDate . '</td>';
+    $html .= '</tr></tbody></table>';
+    return [
+        [
+            'label' => '',
+            'value' => $html,
+        ],
+    ];
+}
 
 /**
  * Implements hook_civicrm_config().
@@ -32,14 +74,14 @@ function mascode_civicrm_config(&$config)
 {
   _mascode_civix_civicrm_config($config);
   
-  if (isset(\Civi::$statics[__FUNCTION__])) return;
-  \Civi::$statics[__FUNCTION__] = 1;
+  // if (isset(\Civi::$statics[__FUNCTION__])) return;
+  // \Civi::$statics[__FUNCTION__] = 1;
 
-  $dispatcher = \Civi::dispatcher();
+  // $dispatcher = \Civi::dispatcher();
 
-  $dispatcher->addListener('hook_civicrm_pre', [new CasePreListener(), 'onPre']);
-  $dispatcher->addListener('hook_civicrm_post', [new CasePostListener(), 'onPost']);
-  $dispatcher->addListener('hook_civicrm_caseSummary', [new CaseSummaryListener(), 'onCaseSummary']);
+  // $dispatcher->addListener('hook_civicrm_pre', [new CasePreListener(), 'onPre']);
+  // $dispatcher->addListener('hook_civicrm_post', [new CasePostListener(), 'onPost']);
+  // $dispatcher->addListener('hook_civicrm_caseSummary', [new CaseSummaryListener(), 'onCaseSummary']);
   // xdebug_break();
 }
 
@@ -53,7 +95,7 @@ function mascode_civicrm_install(): void
   _mascode_civix_civicrm_install();
 
   // Delegate custom installation code to my OOP class
-  \Civi\Mascode\Hook\InstallHook::handle();
+  // \Civi\Mascode\Hook\InstallHook::handle();
 }
 
 /**
@@ -69,10 +111,10 @@ function mascode_civicrm_enable(): void
 /**
  * Implements hook_civicrm_triggerInfo() - required to create CiviRules triggers.
  */
-function mascode_civicrm_triggerInfo(&$triggerInfo)
-{
-  $triggerInfo[] = new \Civi\Mascode\CiviRules\Trigger\MailingUnsubscribe();
-}
+// function mascode_civicrm_triggerInfo(&$triggerInfo)
+// {
+//   $triggerInfo[] = new \Civi\Mascode\CiviRules\Trigger\MailingUnsubscribe();
+// }
 
 // /**
 //  * The rest of this file is not required.  
